179a03609b22043a6fa52e853894f1d94525d29b
jonas18121 <jonasxtrm971@gmail.com>
2020-12-19 20:08:21 +0100

    structuration de l'application

---
+++ src/app/components/book-list/single-book/single-book.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-book',
+  templateUrl: './single-book.component.html',
+  styles: [
+  ]
+})
+export class SingleBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
